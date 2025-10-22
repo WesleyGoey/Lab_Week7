3340d99f608a8330cae5f5c76f6aff57e5442a20
WesleyGoey <wesley.goey@gmail.com>
2025-10-22 13:19:22 +0700

    fgf

--- app/src/main/java/com/wesley/lab_week7/MainActivity.kt
+++ app/src/main/java/com/wesley/lab_week7/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Lab_Week7Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Androido",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
