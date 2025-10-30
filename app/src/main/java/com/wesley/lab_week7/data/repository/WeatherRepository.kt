156223592f07adcb43f908eff9be4d6ab1b8c53b
WesleyGoey <wesley.goey@gmail.com>
2025-10-30 14:59:28 +0700

    p

--- app/src/main/java/com/wesley/lab_week7/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/wesley/lab_week7/data/repository/WeatherRepository.kt
@@ -2,7 +2,6 @@ package com.wesley.lab_week7.data.repository
 
 import com.wesley.lab_week7.data.service.WeatherService
 import com.wesley.lab_week7.ui.model.PanPanWeather
-import java.io.IOException
 
 class WeatherRepository (private val service: WeatherService) {
     suspend fun getWeatherByCity(cityName:String): PanPanWeather{
@@ -10,7 +9,7 @@ class WeatherRepository (private val service: WeatherService) {
             city = cityName,
             apiKey = "c98f91bafdefa4e1a57e2598501305e0",
             units = "metric"
-        ).body()?: throw IOException("Empty response from weather service")
+        ).body()!!
 
         return PanPanWeather(
             city = weather.name,
